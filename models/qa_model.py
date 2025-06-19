67a2a6c5df6cf3417bf0945faf268f6f0103e4f4
Ninad Kothmire <ninadk1012@gmail.com>
2025-06-19 08:52:13 +0100

    created qa model class

---
+++ models/qa_model.py
@@ -0,0 +1,12 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+
+class LocalLLM
+    def __init__(self, model_name="tiiuae/falcon-7b-instruct"):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = model = AutoModelForCausalLM.from_pretrained(model_name)
+        self.pipe = pipeline("test_generation", self.model, tokenizer= self.tokenizer)
+    
+    
+    def answer(self, context, question)
+        prompt = f"Context: {context}\nQuestion: {question}\nAnswer:"
+        return self.pipe(prompt, max_new_tokens = 100)[0]["generated_text"]
